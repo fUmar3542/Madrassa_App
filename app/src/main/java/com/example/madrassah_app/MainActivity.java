014fc41656fcd6c9d6737bb17838a0290e7f4e90
fumar3542 <fumar3542@outlook.com>
2023-01-29 10:44:15 +0500

    Layout components added successfully

--- app/src/main/java/com/example/madrassah_app/MainActivity.java
+++ app/src/main/java/com/example/madrassah_app/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.madrassah_app;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
