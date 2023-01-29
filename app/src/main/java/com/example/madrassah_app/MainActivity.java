6e87c0224d751baf95c0be2044f1a8bb945df69c
fumar3542 <fumar3542@outlook.com>
2023-01-29 11:14:35 +0500

    Components binded successfully

--- app/src/main/java/com/example/madrassah_app/MainActivity.java
+++ app/src/main/java/com/example/madrassah_app/MainActivity.java
@@ -15,10 +15,96 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etName, etRollNo, etSabaq, etSabqi, etManzil;
+    Button btnSave, btnEdit, btnDelete;
+
+    DBHandler db;
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etName = findViewById(R.id.et_name);
+        etRollNo = findViewById(R.id.et_roll_no);
+        etSabaq = findViewById(R.id.et_sabaq);
+        etSabqi = findViewById(R.id.et_sabqi);
+        etManzil = findViewById(R.id.et_manzil);
+        btnSave = findViewById(R.id.btn_save);
+        btnEdit = findViewById(R.id.btn_edit);
+        btnDelete = findViewById(R.id.btn_delete);
 
+        db = new DBHandler(this);
+        listView = findViewById(R.id.list_view);
+        RefreshGrid();
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etName.getText().toString();
+                String rollNo = etRollNo.getText().toString();
+                String sabaq = etSabaq.getText().toString();
+                String sabqi = etSabqi.getText().toString();
+                String manzil = etManzil.getText().toString();
+
+                if (name.isEmpty() || rollNo.isEmpty() || sabaq.isEmpty() || sabqi.isEmpty() || manzil.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter valid data", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Student student = new Student(name, rollNo, sabaq, sabqi, manzil);
+                db.insertStudent(student);
+                RefreshGrid();
+            }
+        });
+
+
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etName.getText().toString();
+                String rollNo = etRollNo.getText().toString();
+                String sabaq = etSabaq.getText().toString();
+                String sabqi = etSabqi.getText().toString();
+                String manzil = etManzil.getText().toString();
+
+                Student student = new Student(name, rollNo, sabaq, sabqi, manzil);
+                db.updateStudent(student);
+                RefreshGrid();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.deleteStudent(etRollNo.getText().toString());
+                Toast.makeText(MainActivity.this, "Student deleted successfully", Toast.LENGTH_SHORT).show();
+                etName.setText("");
+                etRollNo.setText("");
+                etSabaq.setText("");
+                etSabqi.setText("");
+                etManzil.setText("");
+                RefreshGrid();
+            }
+        });
+    }
+    public void RefreshGrid(){
+        List<Student> students = db.selectAllStudents();
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<Student>(MainActivity.this, android.R.layout.simple_list_item_1,students);
+        listView.setAdapter(arrayAdapter);
+
+
+
+//        ArrayAdapter<Student> adapter = new ArrayAdapter<Student>(this, android.R.layout.simple_list_item_1, students){
+//            @Override
+//            public View getView(int position, View convertView, ViewGroup parent) {
+//                View view = super.getView(position, convertView, parent);
+//                TextView text = (TextView) view.findViewById(android.R.id.text1);
+//                text.setText(students.get(position).toString());
+//                return view;
+//            }
+//        };
+//        listView.setAdapter(adapter);
     }
 }
